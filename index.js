de0a0c6a66d9a04d9cf0bf8ad088e71f473c2653
Christopher Rothrie <crothrie@MacBook.local>
2023-10-16 19:39:43 +0100

    Adding bonus points code

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let myAge = 39
 let humanDogRatio = 7
 let myDogAge = myAge * humanDogRatio
 console.log(myDogAge)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
