b260aa4d23ccc147ba24c0624d35542e8ab017b8
Christopher Rothrie <crothrie@MacBook.local>
2023-10-16 20:24:26 +0100

    Passenger counter created

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ bonusPoints = bonusPoints - 75
 console.log(bonusPoints)
 bonusPoints = bonusPoints + 45
 console.log(bonusPoints)
+
+// need a count variable
+// listen for clicks on button
+// increment count
+// update the count on web page
+
+let count = 0;
+
+function increment() {
+    count +=1
+    document.getElementById("count-el").innerText = count
+}
