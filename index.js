723a6591aa112837bca138de3ff1f6a25e1198ee
Christopher Rothrie <crothrie@MacBook.local>
2023-10-19 20:31:41 +0100

    Passenger counter app v2

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ console.log(bonusPoints)
 // update the count on web page
 
 let count = 0;
+let countEl = document.getElementById("count-el")
 
 function increment() {
     count +=1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
+}
+
+function reset() {
+    count = 0;
+    countEl.innerText = count
 }
